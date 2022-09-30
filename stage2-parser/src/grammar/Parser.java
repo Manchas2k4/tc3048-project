94ea59d7758bab8c5fc5d0714f8368170902bfc0
Pedro Perez <pperezm@tec.mx>
2022-09-30 18:59:00 -0500

    parser updated

--- stage2-parser/src/grammar/Parser.java
+++ stage2-parser/src/grammar/Parser.java
@@ -2,6 +2,7 @@ package grammar;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.LinkedHashSet;
 
 import errors.SyntaxError;
 import lexer.Lexer;
@@ -11,9 +12,18 @@ import lexer.Token;
 public class Parser {
 	private Lexer lexer;
 	private Token token;
+	private LinkedHashSet<Tag> firstPrimaryExpression = new LinkedHashSet<Tag>();
+	private LinkedHashSet<Tag> firstUnaryExpression = new LinkedHashSet<Tag>();
 	
 	public Parser (FileInputStream input) {
 		lexer = new Lexer(input);
+		
+		firstPrimaryExpression.add(Tag.ID); firstPrimaryExpression.add(Tag.NUMBER);
+		firstPrimaryExpression.add(Tag.TRUE); firstPrimaryExpression.add(Tag.FALSE);
+		firstPrimaryExpression.add((int) '(');
+		
+		firstUnaryExpression.add((int) '-'); firstUnaryExpression.add((int) '!');
+		firstUnaryExpression.addAll(firstPrimaryExpression);
 	}
 
 	private void check(int tag) throws SyntaxError, IOException {
@@ -26,8 +36,38 @@ public class Parser {
 
 	public void analyze() throws SyntaxError, IOException {
 		token = lexer.scan();
-		if (token.getTag() == Tag.EOF) {
-			System.out.println("ACCEPTED");
+		program();
+	}
+	
+	private void program() {
+		statementSequence();
+	}
+	
+	private void primaryExpression() {
+		if (firstPrimaryExpression.contains(token.getTag())) {
+			switch(token.getTag()) {
+			case Tag.ID : check(Tag.ID) break;
+			case Tag.NUMBER : check(Tag.NUMBER) break;
+			case Tag.TRUE : check(Tag.TRUE) break;
+			case Tag.FALSE : check(Tag.FALSE) break;
+			case ((int) '(') :
+				check((int) '(');
+				expression();
+				check((int) ')');
+				break;
+			}
+		} else {
+			throw new SyntaxError();
+		}
+	}
+	
+	private void unaryExpression() throws SyntaxError, IOException {
+		if (firstUnaryExpression.contains(token.getTag())) {
+			switch(token.getTag()) {
+			case ((int) '-') : check((int) '-'); unaryExpression(); break;
+			case ((int) '!') : check((int) '!'); unaryExpression(); break;
+			default			 : primaryExpression(); break;
+			}
 		} else {
 			throw new SyntaxError();
 		}
