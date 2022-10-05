7662e6e17d7ce57e0a37a864c898334c0d2c86b6
Pedro Pérez <pperezm@tec.mx>
2022-10-05 07:35:59 -0500

    stage2 updated

--- stage2-parser/src/grammar/Parser.java
+++ stage2-parser/src/grammar/Parser.java
@@ -12,8 +12,12 @@ import lexer.Token;
 public class Parser {
 	private Lexer lexer;
 	private Token token;
-	private LinkedHashSet<Tag> firstPrimaryExpression = new LinkedHashSet<Tag>();
-	private LinkedHashSet<Tag> firstUnaryExpression = new LinkedHashSet<Tag>();
+	private LinkedHashSet<Integer> firstPrimaryExpression = new LinkedHashSet<Integer>();
+	private LinkedHashSet<Integer> firstUnaryExpression = new LinkedHashSet<Integer>();
+	private LinkedHashSet<Integer> firstExtendedMultiplicativeExpression = new LinkedHashSet<Integer>();
+	private LinkedHashSet<Integer> firstMultiplicativeExpression = new LinkedHashSet<Integer>();
+	private LinkedHashSet<Integer> firstExtendedAdditiveExpression = new LinkedHashSet<Integer>();
+	private LinkedHashSet<Integer> firstAdditiveExpression = new LinkedHashSet<Integer>();
 	
 	public Parser (FileInputStream input) {
 		lexer = new Lexer(input);
@@ -24,6 +28,15 @@ public class Parser {
 		
 		firstUnaryExpression.add((int) '-'); firstUnaryExpression.add((int) '!');
 		firstUnaryExpression.addAll(firstPrimaryExpression);
+		
+		firstExtendedMultiplicativeExpression.add((int) '*'); firstExtendedMultiplicativeExpression.add((int) '/');
+		firstExtendedMultiplicativeExpression.add(Tag.MOD);
+		
+		firstMultiplicativeExpression.containsAll(firstUnaryExpression);
+		
+		firstExtendedAdditiveExpression.add((int) '+'); firstExtendedAdditiveExpression.add((int) '-');
+		
+		firstAdditiveExpression.addAll(firstMultiplicativeExpression);
 	}
 
 	private void check(int tag) throws SyntaxError, IOException {
@@ -36,23 +49,19 @@ public class Parser {
 
 	public void analyze() throws SyntaxError, IOException {
 		token = lexer.scan();
-		program();
+		//program();
 	}
 	
-	private void program() {
-		statementSequence();
-	}
-	
-	private void primaryExpression() {
+	private void primaryExpression() throws SyntaxError, IOException {
 		if (firstPrimaryExpression.contains(token.getTag())) {
 			switch(token.getTag()) {
-			case Tag.ID : check(Tag.ID) break;
-			case Tag.NUMBER : check(Tag.NUMBER) break;
-			case Tag.TRUE : check(Tag.TRUE) break;
-			case Tag.FALSE : check(Tag.FALSE) break;
+			case Tag.ID : check(Tag.ID); break;
+			case Tag.NUMBER : check(Tag.NUMBER); break;
+			case Tag.TRUE : check(Tag.TRUE); break;
+			case Tag.FALSE : check(Tag.FALSE); break;
 			case ((int) '(') :
 				check((int) '(');
-				expression();
+				//expression();
 				check((int) ')');
 				break;
 			}
@@ -70,6 +79,63 @@ public class Parser {
 			}
 		} else {
 			throw new SyntaxError();
+		}
+	}
+	
+	private void extendedMultiplicativeExpression() throws SyntaxError, IOException {
+		if (firstExtendedMultiplicativeExpression.contains(token.getTag())) {
+			switch(token.getTag()) {
+			case ((int) '*') : 
+				check((int) '*');
+				unaryExpression();
+				extendedMultiplicativeExpression();
+				break;
+			case ((int) '/') : 
+				check((int) '/');
+				unaryExpression();
+				extendedMultiplicativeExpression();
+				break;
+			case Tag.MOD : 
+				check(Tag.MOD);
+				unaryExpression();
+				extendedMultiplicativeExpression();
+				break;
+			}
+		} else {
+			// do nothing
+		}
+	}
+	
+	protected void multiplicativeExpression() throws SyntaxError, IOException {
+		if (firstMultiplicativeExpression.contains(token.getTag())) {
+			unaryExpression();
+			extendedMultiplicativeExpression();
+		}
+	}
+	
+	protected void extendedAdditiveExpression() throws SyntaxError, IOException {
+		if (firstExtendedAdditiveExpression.contains(token.getTag())) {
+			switch(token.getTag()) {
+			case ((int) '+') : 
+				check((int) '+');
+				multiplicativeExpression();
+				extendedAdditiveExpression();
+				break;
+			case ((int) '-') : 
+				check((int) '-');
+				multiplicativeExpression();
+				extendedAdditiveExpression();
+				break;
+			}
+		} else {
+			// do nothing
+		}
+	}
+	
+	protected void additiveExpression() throws SyntaxError, IOException {
+		if (firstAdditiveExpression.contains(token.getTag())) {
+			multiplicativeExpression();
+			extendedAdditiveExpression();
 		}
 	}
 }
